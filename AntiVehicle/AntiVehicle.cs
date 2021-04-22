674bfc99ed6d97878d0cceb70bc5610019d09e8e
Jdance-Media <67397422+Jdance-Media@users.noreply.github.com>
2021-04-22 15:46:38 -0400

    Add files via upload

---
+++ AntiVehicle/AntiVehicle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Steamworks;
+
+namespace AntiVehicle
+{
+    public class AntiVehicle : RocketPlugin<AntiVehicleConfiguration>
+    {
+        protected override void Load()
+        {
+            Logger.Log("AntiVehicle is now loaded!");
+            VehicleManager.onDamageTireRequested += VehicleManager_onDamageTireRequested;
+        }
+
+        private void VehicleManager_onDamageTireRequested(CSteamID instigatorSteamID, InteractableVehicle vehicle, int tireIndex, ref bool shouldAllow, EDamageOrigin damageOrigin)
+        {
+            if (instigatorSteamID != null || vehicle.lockedOwner != null)
+            {
+                var otherPlayer = UnturnedPlayer.FromCSteamID(vehicle.lockedOwner);
+                var unturnedPlayer = UnturnedPlayer.FromCSteamID(instigatorSteamID);
+                Logger.Log($"Hey! {unturnedPlayer.DisplayName} ({unturnedPlayer.CSteamID}) is trying to pop a tire on {otherPlayer.DisplayName}'s vehicle!");
+            }
+        }
+
+        protected override void Unload()
+        {
+            Logger.Log("AntiVehicle is now unloaded!");
+        }
+    }
+}
---
+++ AntiVehicle/AntiVehicleConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.API;
+
+namespace AntiVehicle
+{
+    public class AntiVehicleConfiguration : IRocketPluginConfiguration
+    {
+        public void LoadDefaults()
+        {
+        }
+    }
+}
